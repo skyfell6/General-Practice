50f42c57c2413303fe17a188e36ceb6790d51ab8
Mike <totafell@gmail.com>
2016-09-21 15:11:39 -0400

    Add files via upload

---
+++ ArrayPracOne.java
@@ -0,0 +1,23 @@
+// head first java page 59 practice 9/21/2016
+public class ArrayPracOne{
+   public static void main(String[] args){
+      int[] nums;
+      nums = new int[8];
+      nums[0] = 25; 
+      nums[1] = 10;
+      nums[2] = 40;
+      nums[3] = 103;
+      nums[4] = 67;
+      nums[5] = 49;
+      nums[6] = 52; 
+      nums[7] = 51;
+      System.out.println("num 1 = "+nums[0]);
+      System.out.println("num 2 = "+nums[1]);
+      System.out.println("num 3 = "+nums[2]);
+      System.out.println("num 4 = "+nums[3]);
+      System.out.println("num 5 = "+nums[4]);
+      System.out.println("num 6 = "+nums[5]);
+      System.out.println("num 7 = "+nums[6]);
+      System.out.println("num 8 = "+nums[7]);
+   }
+}
